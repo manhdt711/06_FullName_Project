9f2553211d6c62c9dc00537c8c53c2954c790b49
thanhnc9422 <thanhnche160277@fpt.edu.vn>
2024-03-14 14:04:29 +0700

    optimize code for order screen

---
+++ 06_FullName_Project/Controllers/OrdersController.cs
@@ -0,0 +1,89 @@
+﻿using ElecStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _06_FullName_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly ElectricStore1Context _context;
+        public OrdersController(ElectricStore1Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetAllOrder")]
+        public ActionResult<List<ClassLibrary2.DTO.OrderDTO>> GetAllOrder()
+        {
+            List<ClassLibrary2.DTO.OrderDTO> orders = _context.Orders.Select(x => 
+            new ClassLibrary2.DTO.OrderDTO {
+                commodityId = x.CommodityId, 
+                commodityName = x.Commodity.CommodityName, 
+                //quantity = x.OrderDetails.ToList()[0].Quantity, 
+                //price = x.OrderDetails.ToList()[0].UnitPrice,
+                customerName = x.Customer.CustomerName, 
+                address = x.Customer.CustomerAddress, 
+                phoneNumber = x.Customer.CustomerPhone, 
+                note = x.Customer.Comment,
+                userId = x.UserId,
+                status = x.PaymentMethod,
+                orderId = x.OrderId,
+            }).ToList();
+            return orders;
+        }
+        [HttpPost("AddOrder")]
+        public ActionResult AddOrder([FromBody] ClassLibrary2.DTO.OrderDTO orderDTO)
+        {
+            // Get the current local time
+            DateTime currentTime = DateTime.Now;
+
+            // Get the current UTC time
+            DateTime currentUtcTime = DateTime.UtcNow;
+            Date date = new Date()
+            {
+                Year = currentTime.Year,
+                Month = currentTime.Month,
+                Day = currentTime.Day,
+            };
+            //_context.Dates.Add(date);
+            //_context.SaveChanges();
+            List<OrderDetail> orderDetail = new List<OrderDetail>();
+            orderDetail.Add(new OrderDetail()
+            {
+                UnitPrice = orderDTO.price,
+                Quantity = orderDTO.quantity,
+                TotalPrice = orderDTO.price * orderDTO.quantity
+            });
+
+
+            Customer customer = new Customer()
+            {
+                CustomerAddress = orderDTO.address,
+                CustomerPhone = orderDTO.phoneNumber,
+                Comment = orderDTO.note,
+                CustomerName = orderDTO.customerName
+            };
+
+            //_context.Customers.Add(customer);
+            //_context.SaveChanges();
+
+            Order order = new Order()
+            {
+                CommodityId = orderDTO.commodityId,
+                //CustomerId = customer.CustomerId,
+                //DateId = date.DateId,
+                PaymentMethod = "pending",
+                PricedProducts = (int)orderDTO.price,
+                Date = date,
+                Customer = customer,
+                OrderDetails = orderDetail,
+                UserId = orderDTO.userId
+            };
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ 06_FullName_Project_WebApp/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace _06_FullName_Project_WebApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient client = null;
+        private string url = "";
+        public OrderController()
+        {
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+            url = "https://localhost:7194/api/User";
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}

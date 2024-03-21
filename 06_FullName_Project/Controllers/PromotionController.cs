b0f84d2c227ec7d1ed454b8acd330467762d5d58
thanhnc9422 <thanhnche160277@fpt.edu.vn>
2024-03-21 22:42:08 +0700

    add screen for promotion

---
+++ 06_FullName_Project/Controllers/PromotionController.cs
@@ -0,0 +1,48 @@
+﻿using ClassLibrary2.ViewModel;
+using ElecStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _06_FullName_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PromotionController : ControllerBase
+    {
+        private readonly ElectricStore1Context _context;
+        public PromotionController(ElectricStore1Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetAllPromotions")]
+        public ActionResult<IEnumerable<ElecStore.Models.Promotion>> GetAllPromotions()
+        {
+           return _context.Promotions.ToList();
+        }
+        [HttpGet("Delete/{id}")]
+        public IActionResult DeletePromotion(int id)
+        {
+            Promotion promotion = _context.Promotions.FirstOrDefault(x => x.PromotionId == id);
+            if (promotion != null)
+            {
+                _context.Promotions.Remove(promotion);
+                _context.SaveChanges();
+            }
+            return NoContent();
+        }
+        [HttpPost("Update")]
+        public IActionResult UpdatePromotion(Promotion c)
+        {
+            _context.Promotions.Add(c);
+            _context.SaveChanges();
+            return NoContent();
+        }
+        [HttpPost("Create")]
+        public IActionResult CreatePromotion(Promotion c)
+        {
+            _context.Promotions.Add(c);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}

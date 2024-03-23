b8b76d3b139d468db463d813e556eff4ce4ccdc2
manhdt711 <dtm.it2002@gmail.com>
2024-03-23 22:08:39 +0700

    UpdateExcel

--- 06_FullName_Project_WebApp/Controllers/CommodityController.cs
+++ 06_FullName_Project_WebApp/Controllers/CommodityController.cs
@@ -13,6 +13,8 @@ using ElecStore.Models;
 using DocumentFormat.OpenXml.InkML;
 using DocumentFormat.OpenXml.Spreadsheet;
 using ClassLibrary2.ViewModel;
+using OfficeOpenXml;
+using ClassLibrary2.DTO;
 
 namespace eStoreClient.Controllers
 {
@@ -78,23 +80,35 @@ namespace eStoreClient.Controllers
 
         public async Task<IActionResult> ExportToExcel()
         {
-     
+
             HttpResponseMessage respone = await client.GetAsync($"{ApiCateUrl}/GetCommodityManager");
             string strData = await respone.Content.ReadAsStringAsync();
             var options = new JsonSerializerOptions
             {
                 PropertyNameCaseInsensitive = true
             };
-            List<Commodity> listCommoditys = JsonSerializer.Deserialize<List<Commodity>>(strData, options); */
+            List<ClassLibrary2.DTO.CommodityDTO> listCommodities = JsonSerializer.Deserialize<List<CommodityDTO>>(strData, options);
+
+            // Tạo một ExcelPackage mới
             using (var excelPackage = new ExcelPackage())
             {
+                // Tạo một worksheet mới
                 var worksheet = excelPackage.Workbook.Worksheets.Add("Commodities");
 
-                worksheet.Cells.LoadFromCollection(commodities, true);
+                // Load dữ liệu từ danh sách vào worksheet
+                worksheet.Cells.LoadFromCollection(listCommodities, true);
 
+                // Thêm cột filter vào worksheet
+                worksheet.Cells[worksheet.Dimension.Address].AutoFilter = true;
+
+                // Lưu workbook vào một MemoryStream
                 var memoryStream = new MemoryStream();
                 excelPackage.SaveAs(memoryStream);
 
+                // Đặt vị trí của memoryStream ở đầu
+                memoryStream.Position = 0;
+
+                // Trả về file Excel như một file tải về
                 return File(memoryStream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Commodities.xlsx");
             }
         }
